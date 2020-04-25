ae89d36b564a0e0956fd6174016cda2a7fe2fdc6
aliaksandr-bandziuk <aliaksandr.bandziuk@gmail.com>
2020-04-25 16:04:46 +0300

    else

--- script/script.js
+++ script/script.js
@@ -1,11 +1,17 @@
-'use strict';
-
 window.addEventListener('DOMContentLoaded', function() {
+    'use strict';
     // Timer
     function countTimer(deadline){
         let hours = document.querySelector('#timer-hours'),
             minutes = document.querySelector('#timer-minutes'),
-            seconds = document.querySelector('#timer-seconds');
+            seconds = document.querySelector('#timer-seconds'),
+            // переменная для вычисления разницы дат
+            // то есть получаем конечную дату
+            dateStop = new Date(deadline).getTime(),
+            // получаем текущую дату
+            dateNow = new Date().getTime();
+            console.log(dateNow);
+            console.log(dateStop);
     }
 
     console.log('01 july 2019');
