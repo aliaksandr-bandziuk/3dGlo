9a8d76baa806cd114b4853963a156df7796c2bf2
aliaksandr-bandziuk <aliaksandr.bandziuk@gmail.com>
2020-04-25 15:50:35 +0300

    upd

---
+++ script/script.js
@@ -0,0 +1,12 @@
+'use strict';
+
+window.addEventListener('DOMContentLoaded', function() {
+    // Timer
+    function countTimer(deadline){
+        let hours = document.querySelector('#timer-hours'),
+            minutes = document.querySelector('#timer-minutes'),
+            seconds = document.querySelector('#timer-seconds');
+    }
+
+    console.log('01 july 2019');
+});
